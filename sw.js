655388f8d55ca743b2a29d2abf5564a874b3a803
takimhd9 <126906639+takimhd9@users.noreply.github.com>
2026-08-20 21:08:26 +0000

    Cache shell v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "trails-shell-v13";
+const C = "trails-shell-v14";
 const SHELL = ["./", "./index.html", "./articles.json", "./collections.json", "./collections/taki.json", "./collections/luna.json", "./collections/mindbend.json", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(C).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
