f9873f926992624e6c47fdcccd5a0a6a532d6fbd
takimhd9 <126906639+takimhd9@users.noreply.github.com>
2026-08-20 15:42:03 +0000

    Bump shell cache to v6 with Luna

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C = "trails-shell-v5";
-const SHELL = ["./", "./index.html", "./articles.json", "./collections.json", "./collections/taki.json", "./manifest.webmanifest", "./icon.svg"];
+const C = "trails-shell-v6";
+const SHELL = ["./", "./index.html", "./articles.json", "./collections.json", "./collections/taki.json", "./collections/luna.json", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(C).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
 });
