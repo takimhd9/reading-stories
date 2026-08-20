7558096256cab96e9f6615dc5adfeddbe0b4e6cb
takimhd9 <126906639+takimhd9@users.noreply.github.com>
2026-08-20 15:09:10 +0000

    Add app-shell service worker

---
+++ sw.js
@@ -0,0 +1,19 @@
+const C = "trails-shell-v1";
+const SHELL = ["./", "./index.html", "./articles.json", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(C).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== C).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (e) => {
+  const u = new URL(e.request.url);
+  if (u.origin !== self.location.origin) return;
+  e.respondWith(
+    fetch(e.request).then((r) => {
+      const copy = r.clone();
+      caches.open(C).then((c) => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
